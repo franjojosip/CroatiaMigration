64962774cdc2737ffa42585477802c415dde5ed0
franjojosip <franjojosip.jukic2@gmail.com>
2020-07-05 14:30:17 +0200

    Change script links

--- info.php
+++ info.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prirodno Kretanje Stanovništva</title>
-    <link rel="stylesheet" type="text/css" href="style/info.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/style/info.css" media="screen" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="shortcut icon" href="/images/main.png">
     <script src="https://d3js.org/d3.v4.min.js"></script>
     <script src="https://d3js.org/topojson.v1.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <link rel="shortcut icon" href="images/main.png">
 </head>
 
 <body onload="sizeChange()">
